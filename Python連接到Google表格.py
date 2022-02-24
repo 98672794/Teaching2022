130d90a611a2662a00116d34f7f05409889b9616
mokaki <95267354+98672794@users.noreply.github.com>
2022-02-24 20:00:26 +0800

    Add files via upload

--- Python連接到Google表格.py
+++ Python連接到Google表格.py
@@ -14,6 +14,20 @@
 #!/usr/bin/python
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 引用庫
 import os
 from copy import Error
@@ -37,24 +51,92 @@ except ImportError:     # 如不能 import , install pygsheets庫
     OK = '_ATWImportThis = Error'
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 # Start = 加內容在這 
 def Start2022():
+
+    Talk = '''
+            ****************
+        將 Python 連接到 Google 表格 
+        歡迎您 
+
+        請將您的 金鑰json 放在同文件夾
+        和準備好 Google 表格 網址
+
+        *您必須先授權表格給服務帳戶
+        
+        說明
+        https://github.com/98672794/Teaching2022/blob/main/6.1%20如何將%20Python連接到Google表格.md
+    
+        '''
+
     # try = 嚐試執行以下代碼, 如錯誤執行except
     try:
-        print('寫在這裡')
+
         NowOK = 'Start2022() = OK'
         ######################################################################
         ####################### Code 寫在這裡 ################################
         ######################################################################
 
-        # Python 連接到 Google 表格 
-        Your金鑰json = 'key.json'   # 轉成您的
-        YourGoogleSheetUrl = 'https://docs.google.com/spreadsheets/d/134Njkg1FaTOjVPxq_clDwKEK1_nPIPVrMpAPfcPJldU/' # 轉成您的
+        # index
+        print(Talk)
+        while True: # 不能空值loo
+            Your金鑰json = input('請填寫金鑰json文件名\n*必須放在同文件夾\n(不需要 .json)')
+            if Your金鑰json == '': continue    # 不能空值
 
+            YourGoogleSheetUrl = input('請填寫Google 表格網址\n*您必須先授權表格給服務帳戶\n')
+            if YourGoogleSheetUrl == '': continue    # 不能空值
+
+            break
+        Your金鑰json = Your金鑰json + '.json'   # 加副檔名
+
+
+        # 連接到 Google 表格
         gc = pygsheets.authorize(service_file=Your金鑰json)     # gc 這邊是告訴 Python 我們的授權金鑰 json 放置的位子
         sht = gc.open_by_url(YourGoogleSheetUrl)        # 利用 Python 開啟 GoogleSheet
-        print('取得此GoogleSheet內的工作表')
+
+        print('##################\n文件\n',YourGoogleSheetUrl,'\n內的工作表目錄\n')
         print(sht.worksheets())     # 取得此GoogleSheet內的工作表
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     # 返回 _Error function 的內容
@@ -64,6 +146,28 @@ def Start2022():
         NowOK = 'Start2022() = Error'
     # 返回 NowOK
     return NowOK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,6 +181,37 @@ def _Error(e):
         '========= \ ==========='
     ]
     return 異常內容
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 '''
