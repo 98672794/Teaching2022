f97f7eb2ceb0865996f9f3bd446a5df12f6febd1
mokaki <95267354+98672794@users.noreply.github.com>
2022-02-26 17:09:39 +0800

    Create joey202202.py

---
+++ joey202202.py
@@ -0,0 +1,19 @@
+
+
+
+- [開發 software/工具 Windows based, 常用/必需/建議 ](https://github.com/98672794/Teaching2022/blob/main/2%20windows%E9%96%8B%E7%99%BC%E5%B7%A5%E5%85%B7%E4%B8%8B%E8%BC%89%E5%AE%89%E8%A3%9D.md)
+
+
+- 寫 program 流程, 如: 如何選擇design pattern, 如何 debug, etc
+- 何謂 Framework? Python 常用 Framework? 
+- 現時最普遍的 database sevice, 我只用過 mysql 及 mariaDB
+
+
+- 應該如何設計 database 
+- 如何用 Python 開發 web application?  如何用 Python 開發 desktop application?
+- Python 連接 database, 對databse作出操作
+- Python 經 database 資料產生 xlsx, jpeg 及 pdf 檔案
+
+依家我自己用 php+mariaDB 寫咗個 web application, 係一個設備報表系統, 有啲似 invoice system. 大致包括, 用戶管理(有不同權限), 分店管理(分店只可處理自己用戶嘅報表), 設備資料管理, 報表輸出(xlsx), 設備catalogue整合成zip檔下載, 供稱商聯絡資料, 等功能
+
+希望能以 Python 取代 PHP web applcaiton, 甚至以此基礎開發成獨立 Desktop Application.
